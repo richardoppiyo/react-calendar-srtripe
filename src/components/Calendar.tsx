d3fcd81bda351947c4bc68e082681d305f6dabef
Richard Opiyo <richardoppiyo@gmail.com>
2022-06-27 13:43:01 +0300

    configs

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,13 +1,30 @@
-import React from "react";
-import  { Weekday, Date } from '../../types';
+import React, { useState, MouseEvent, useRef} from "react";
+import  { Date } from '../../types';
 import { Weekdays } from "../configs/Weekdays";
 import { monthDates } from "../configs/MonthDays";
+import { BsChevronRight, BsChevronLeft } from "react-icons/bs";
+
 
 const Calendar : React.FC<{}> = ({}) => {
+    const [selectedDate, setSelectedDate] = useState<string | null >();
+    const inputRef = useRef(0);
+
+    const handleChange = (e: MouseEvent<HTMLButtonElement>) => {
+        setSelectedDate(e.currentTarget.getAttribute('value'));
+    }
+
+      const scroll = (scrollOffset: number) => {
+        // To handle scroll of the element
+        inputRef.current += scrollOffset;
+        console.log(inputRef);
+      };
+
 
     const generateDates= (date: number) => {
+        let selectedDateNumber: number = selectedDate ? parseInt(selectedDate) : 0;
         for(let i=0 ; i< 7 ; i++) {
-                return <button className="date" value= {date}><p>{date}</p></button>
+               console.log(<button className={`date ${date === 4 ? "today": ""} ${date === selectedDateNumber ? "selected" : ""}`} onClick={handleChange} value= {date}><p>{date}</p></button>);
+                return <button className={`date ${date === 4 ? "today": ""} ${date === selectedDateNumber ? "selected" : ""}`} onClick={handleChange} value= {date}><p>{date}</p></button> 
         }
     }
 
@@ -21,11 +38,12 @@ const Calendar : React.FC<{}> = ({}) => {
         return tempArray;
     }
 
-
-
     return (
         <div className="calendar-container">
             <div className="datepicker-container">
+                <BsChevronLeft  onClick={() => scroll(-7)}/>
+                <span>Selected date: {selectedDate}</span>
+                <BsChevronRight onClick={() => scroll(7)} />
             </div>
 
             <div className="weekdays-container">
